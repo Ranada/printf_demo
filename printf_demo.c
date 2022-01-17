8e6ef34449ba97dea3b88beb9db7e4a88b353492
Neil Ranada <neil.ranada@gmail.com>
2022-01-17 14:41:38 -0800

    Added loop with switch statements

--- printf_demo.c
+++ printf_demo.c
@@ -3,21 +3,51 @@
 #include <stdarg.h>
 #include <string.h>
 
-int printf_demo(const char* format, ...)
+int my_printf(const char* format, ...)
 {
-    va_list args;
-    int done;
+    va_list args_pointer; // Points to each agrument in the list
+    const char* pointer_value;
+    char* string_value;
+    int int_value;
+    double double_value;
+    int done = 0;
 
-    va_start(args, format);
-    done = vfprintf(stdout, format, args);
-    va_end(args);
+    va_start(args_pointer, format); // Looks at the first REQUIRED argument then finds and point to the first OPTIONAL argument
+
+    for (pointer_value = format; *pointer_value; pointer_value++)
+    {
+        if (*pointer_value != '%')
+        {
+            putchar(*pointer_value);
+            continue;
+        }
+        switch (*++pointer_value)
+        {
+            case 'd':
+                int_value = va_arg(args_pointer, int);
+                printf("%d", int_value);
+                break;
+            case 'f':
+                double_value = va_arg(args_pointer, double);
+                printf("%f", double_value);
+                break;
+            case 's':
+                for (string_value = va_arg(args_pointer, char*);  *string_value; string_value++)
+                    putchar(*string_value);
+                break;
+            default:
+                putchar(*pointer_value);
+                break;
+        }
+    }
+    va_end(args_pointer); //Clean up argument list
 
     return done;
 }
 
-int main()
-{
-    printf_demo("Hello World.\n");
-
-    return 0;
-}
+// int main()
+// {
+//     my_printf("hello world.\n");
+    
+//     return 0;
+// }
--- printf_demo.h
+++ printf_demo.h
@@ -1,6 +1,7 @@
 #ifndef PRINTF_DEMO_H
 #define PRINTF_DEMO_H
 
-int printf_demo(const char* format, ...);
+int my_printf(const char* format, ...);
+//int get_input_length(char string[], int n);
 
 #endif //PRINTF_DEMO_H
